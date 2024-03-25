2bfc5702034a1a88924ea0546594a577ce8a3f58
ShivaPrasad03 <shivacherukunuri@gmail.com>
2024-03-25 16:05:33 +0530

    updated

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./model/user_model");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 mongoose
   .connect(
     `mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@atlascluster.ci2lv2t.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`
@@ -21,38 +21,44 @@ app.get("/", (req, res) => {
 
 app.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
-  const bool = await User.findOne({email})
-  if(bool){
-  return res.status(400).send({message:"This email is already registered with Ditto."})
+  const bool = await User.findOne({ email });
+  if (bool) {
+    return res
+      .status(400)
+      .send({ message: "This email is already registered with Ditto." });
   }
   const new_data = new User({ username, email, password });
   new_data.save();
-  return res.status(200).send({message:"Your Ditto Account Created Successfully!!"});
+  return res
+    .status(200)
+    .send({ message: "Your Ditto Account Created Successfully!!" });
 });
 
-app.post("/login", async(req, res)=>{
-  const {email, password}= req.body
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-  const data = await User.findOne({email})
+  const data = await User.findOne({ email });
 
-  if(!data){
-    return res.status(401).send("The Details You Entered Are Incorrect")
+  if (!data) {
+    return res
+    .status(401)
+    .send({message:"The Details You Entered Are Incorrect"});
   }
-  if(password!==data.password){
-    return res.status(401).send("The Details You Entered Are Incorrect")
+  if (password !== data.password) {
+    return res.status(401).send({message:"The Password You Entered is Incorrect"});
   }
-  const payload={
-    user:{
-      id:data.id
+  const payload = {
+    user: {
+      id: data.id,
+    },
+  };
+  jwt.sign(payload, "jwtToken", { expiresIn: 3600000 }, (err, token) => {
+    if (err) {
+      return res.status(400).send(err);
     }
-  }
-  jwt.sign(payload, "jwtToken", {expiresIn:3600000},
-  (err,token)=>{
-    if(err){
-  return res.status(400).send(err)
-    }
-    return res.send({token})
-  })
-})
+    return res.send({ token });
+  });
+
+});
 
 app.listen(3001, () => console.log("server started"));
